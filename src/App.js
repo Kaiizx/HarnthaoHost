091c839c5fa74798775880a6e5c9108a31d48981
AfnanLeewan <naaanlw22@gmail.com>
2023-02-03 23:27:51 +0700

    credit

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     <div className={classes.App}>
         <Header/>
         <Card order={dummy} amount={orderAmount} person={dummyperson}/>
+        <footer><p>Harnthao1.0</p><p>Create By Afnan</p></footer>
     </div>
   );
 }
